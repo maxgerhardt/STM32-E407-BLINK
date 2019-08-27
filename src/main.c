cae5eef2062a29bb453f4962db7371037419fe22
Maximilian Gerhardt <maxi.gerhardt@googlemail.com>
2019-08-28 01:08:44 +0200

    Change example to STM32Cube

---
+++ src/main.c
@@ -0,0 +1,132 @@
+/*
+ * S T M 3 2 - E 4 0 7    B L I N K
+ *
+ * Copyright (C) 2019  Nicolas Jeanmonod, ouilogique.com
+ *
+ */
+
+#include "stm32f4xx_hal.h"
+
+#define LED_PIN                                GPIO_PIN_5
+#define LED_GPIO_PORT                          GPIOA
+#define LED_GPIO_CLK_ENABLE()                  __HAL_RCC_GPIOA_CLK_ENABLE()
+#define LED2_PIN                                GPIO_PIN_13
+#define LED2_GPIO_PORT                          GPIOC
+#define LED2_GPIO_CLK_ENABLE()                  __HAL_RCC_GPIOC_CLK_ENABLE()
+
+void SystemClock_Config(void);
+
+int main(void)
+{
+  HAL_Init();
+
+  SystemClock_Config();
+  SystemCoreClockUpdate();
+
+  LED_GPIO_CLK_ENABLE();
+  LED2_GPIO_CLK_ENABLE();
+
+  GPIO_InitTypeDef GPIO_InitStruct;
+
+  GPIO_InitStruct.Pin = LED_PIN;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+  HAL_GPIO_Init(LED_GPIO_PORT, &GPIO_InitStruct);
+
+  GPIO_InitStruct. Pin = LED2_PIN;
+  HAL_GPIO_Init(LED2_GPIO_PORT, &GPIO_InitStruct);
+
+  while (1)
+  {
+    HAL_GPIO_TogglePin(LED_GPIO_PORT, LED_PIN);
+    HAL_GPIO_TogglePin(LED2_GPIO_PORT, LED2_PIN);
+
+    HAL_Delay(200);
+  }
+}
+
+void SysTick_Handler(void)
+{
+  HAL_IncTick();
+}
+
+void NMI_Handler(void)
+{
+}
+
+void HardFault_Handler(void)
+{
+  while (1) {}
+}
+
+
+void MemManage_Handler(void)
+{
+  while (1) {}
+}
+
+void BusFault_Handler(void)
+{
+  while (1) {}
+}
+
+void UsageFault_Handler(void)
+{
+  while (1) {}
+}
+
+void SVC_Handler(void)
+{
+}
+
+
+void DebugMon_Handler(void)
+{
+}
+
+void PendSV_Handler(void)
+{
+}
+
+void Error_Handler() { while(1) {} }
+
+/* bootup from HSI */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+  /** Initializes the CPU, AHB and APB busses clocks
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = 8;
+  RCC_OscInitStruct.PLL.PLLN = 168;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 7;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Initializes the CPU, AHB and APB busses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
